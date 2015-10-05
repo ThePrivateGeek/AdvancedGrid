bc2c85a17c951f4d9a56aa232a7fde63f70d4e42
TPG <admin@theprivategeek.com>
2015-10-05 08:50:53 +0200

    Some more code samples

--- TEST/formTest.Designer.vb
+++ TEST/formTest.Designer.vb
@@ -22,39 +22,73 @@ Partial Class formTest
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(formTest))
         Me.AdvanceGrid1 = New AdvanceGrid.AdvanceGrid()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'AdvanceGrid1
         '
-        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.AdvanceGrid1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        DataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.AdvanceGrid1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle3
         Me.AdvanceGrid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithAutoHeaderText
         Me.AdvanceGrid1.DataMember = ""
         Me.AdvanceGrid1.DataSource = Nothing
-        Me.AdvanceGrid1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.AdvanceGrid1.Location = New System.Drawing.Point(0, 0)
         Me.AdvanceGrid1.Name = "AdvanceGrid1"
-        Me.AdvanceGrid1.RowsDefaultCellStyle = DataGridViewCellStyle2
+        Me.AdvanceGrid1.RowsDefaultCellStyle = DataGridViewCellStyle4
         Me.AdvanceGrid1.SearchColor = System.Drawing.Color.LemonChiffon
         Me.AdvanceGrid1.SearcheValues = CType(resources.GetObject("AdvanceGrid1.SearcheValues"), System.Collections.Generic.Dictionary(Of String, String))
         Me.AdvanceGrid1.Size = New System.Drawing.Size(469, 314)
         Me.AdvanceGrid1.TabIndex = 0
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(484, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(233, 112)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(484, 130)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(233, 112)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(642, 291)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'formTest
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(469, 314)
+        Me.ClientSize = New System.Drawing.Size(729, 362)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.AdvanceGrid1)
         Me.Name = "formTest"
         Me.Text = "Test Form"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents AdvanceGrid1 As AdvanceGrid.AdvanceGrid
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- TEST/formTest.vb
+++ TEST/formTest.vb
@@ -3,11 +3,15 @@
 
 
     Private Sub formTest_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Create a new Data Table
         Dim DT As New DataTable
+
+        'Add some columns
         DT.Columns.Add("Col01_String", GetType(String))
         DT.Columns.Add("Col02_Integer", GetType(Integer))
         DT.Columns.Add("Col03_Boolean", GetType(Boolean))
 
+        'Add some rows
         DT.Rows.Add("France", 4500, True)
         DT.Rows.Add("Germny", 5100, True)
         DT.Rows.Add("UK", 9515, False)
@@ -15,9 +19,32 @@
         DT.Rows.Add("Italy", 6730, False)
         DT.Rows.Add("Norway", 1500, True)
 
+        'Bind the AdvanceGrid's data source to your table
         AdvanceGrid1.DataSource = DT
 
-        MessageBox.Show("Done loading")
+        AdvanceGrid1.Dock = DockStyle.Left
 
     End Sub
+
+
+    Private Sub AdvanceGrid1_TextChanged(sender As Object) Handles AdvanceGrid1.TextChanged
+
+        'Display the filter in TextBox1
+        TextBox1.Text = AdvanceGrid1.Filter
+
+        'Diplay the Search values in TextBox2
+        TextBox2.Text = ""
+        For Each SearchValue As KeyValuePair(Of String, String) In AdvanceGrid1.SearcheValues
+            TextBox2.Text &= SearchValue.Key & ":" & SearchValue.Value & vbCrLf
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Programaticaly search the grid by assigning values to one or more columns and calling the search function.
+        AdvanceGrid1.SearcheValues("Col01_String") = "France"
+        AdvanceGrid1.Search()
+    End Sub
+
+
+
 End Class
